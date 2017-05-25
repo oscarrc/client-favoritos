58d4e13ee93f6f9b322bead411416935fb133e0f
Oscar R.C <oscarrc.web@gmail.com>
2017-05-25 18:18:08 +0200

    Crear modelo de datos

---
+++ app/models/favorito.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+//Exportamos la clase
+var Favorito = (function () {
+    //Definimos el constructor
+    function Favorito(title, url, description) {
+        this.title = title;
+        this.url = url;
+        this.description = description;
+    }
+    return Favorito;
+}());
+exports.Favorito = Favorito;
+//# sourceMappingURL=favorito.js.map
---
+++ app/models/favorito.ts
@@ -0,0 +1,9 @@
+//Exportamos la clase
+export class Favorito{
+    //Definimos el constructor
+    constructor(
+        public title: string,
+        public url: string,
+        public description: string
+    ){}
+}
